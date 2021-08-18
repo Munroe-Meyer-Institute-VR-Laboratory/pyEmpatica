aacf079baa3ec049832fa85f286af65054c88ffe
Walker Arce <wsarcera@gmail.com>
2021-08-18 18:36:10 -0500

    Created Python package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='pyEmpatica',
+      version='0.1',
+      url='https://github.com/Munroe-Meyer-Institute-VR-Laboratory/pyEmpatica',
+      author='Walker Arce',
+      author_email='walker.arce@unmc.edu',
+      description='Communicate with your Empatica E4 in your Python scripts.',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      zip_safe=False)
+
+# python setup.py bdist_wheel
+# pip3 install pdoc3
+# pdoc --html --output-dir docs pyempatica
